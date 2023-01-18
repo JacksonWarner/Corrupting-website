418ac1db00f1f3856b6bc16a3f52ebcf1088dd06
rvpatel5-asu <112120914+rvpatel5-asu@users.noreply.github.com>
2023-01-17 19:07:01 -0700

    Update app.js

--- Capstone_Website/app.js
+++ Capstone_Website/app.js
@@ -1,10 +1,31 @@
-const textz = "pTextIcon";
 const testBtn = document.getElementById("testButton");
+const imagesBtn = document.getElementById("imagesButton");
+const stylesBtn = document.getElementById("stylesButton");
+
+let recoverProtocol = false;
+
 
 window.onload=function(){
     var textIcon = document.getElementById("pTextIcon");
+    var imagesIcon = document.getElementById("pImagesIcon");
+    var stylesIcon = document.getElementById("pStylesIcon");
+
+
     testBtn.addEventListener("click", function() {
         textIcon.className = "fa-solid fa-check";
+        console.log(textIcon.className);
+       // protocolCheck();
     }, false);
-  }
 
+        imagesBtn.addEventListener("click", function() {
+        imagesIcon.className = "fa-solid fa-check";
+        console.log(imagesIcon.className);
+       // protocolCheck();
+    }, false);
+
+    stylesBtn.addEventListener("click", function() {
+        stylesIcon.className = "fa-solid fa-check";
+        console.log(stylesIcon.className);
+        //protocolCheck();
+    }, false); 
+  }
