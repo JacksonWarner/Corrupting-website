4435acb4345d20aec6e9547670496e9dfd34e2cf
rvpatel5-asu <112120914+rvpatel5-asu@users.noreply.github.com>
2023-01-19 20:07:15 -0700

    Update app.js

--- Capstone_Website/app.js
+++ Capstone_Website/app.js
@@ -1,31 +1,72 @@
 const testBtn = document.getElementById("testButton");
 const imagesBtn = document.getElementById("imagesButton");
 const stylesBtn = document.getElementById("stylesButton");
+var textIcon = document.getElementById("pTextIcon");
+var imagesIcon = document.getElementById("pImagesIcon");
+var stylesIcon = document.getElementById("pStylesIcon");
+var recoverBtn = document.getElementById("recoverBtn");
 
 let recoverProtocol = false;
 
+document.querySelector('#recoverBtn').disabled = true;
 
 window.onload=function(){
-    var textIcon = document.getElementById("pTextIcon");
-    var imagesIcon = document.getElementById("pImagesIcon");
-    var stylesIcon = document.getElementById("pStylesIcon");
-
-
     testBtn.addEventListener("click", function() {
         textIcon.className = "fa-solid fa-check";
         console.log(textIcon.className);
-       // protocolCheck();
+        if(recoverProtocol == false){
+            if(textIcon.className == "fa-solid fa-check"){
+                if(imagesIcon.className == "fa-solid fa-check"){
+                    if(stylesIcon.className == "fa-solid fa-check"){
+                        recoverProtocol = true;
+                        recoverBtn.style.backgroundColor = "green";
+                        console.log("recoverProtocol");
+                        document.querySelector('#recoverBtn').disabled = false;
+                    } 
+                } 
+            }
+        }else{
+            recoverProtocol = false;
+        }
     }, false);
 
         imagesBtn.addEventListener("click", function() {
         imagesIcon.className = "fa-solid fa-check";
         console.log(imagesIcon.className);
-       // protocolCheck();
+        if(recoverProtocol == false){
+            if(textIcon.className == "fa-solid fa-check"){
+                if(imagesIcon.className == "fa-solid fa-check"){
+                    if(stylesIcon.className == "fa-solid fa-check"){
+                        recoverProtocol = true;
+                        recoverBtn.style.backgroundColor = "green";
+                        console.log("recoverProtocol");
+                        document.querySelector('#recoverBtn').disabled = false; 
+                    } 
+                } 
+            }
+        }else{
+            recoverProtocol = false;
+        }
     }, false);
 
     stylesBtn.addEventListener("click", function() {
         stylesIcon.className = "fa-solid fa-check";
         console.log(stylesIcon.className);
-        //protocolCheck();
-    }, false); 
+        if(recoverProtocol == false){
+            if(textIcon.className == "fa-solid fa-check"){
+                if(imagesIcon.className == "fa-solid fa-check"){
+                    if(stylesIcon.className == "fa-solid fa-check"){
+                        recoverProtocol = true;
+                        recoverBtn.style.backgroundColor = "green";
+                        console.log("recoverProtocol");
+                        document.querySelector('#recoverBtn').disabled = false;
+
+                    } 
+                } 
+            }
+        }else{
+            recoverProtocol = false;
+        }
+    }, false);
+
   }
